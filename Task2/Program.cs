3659d8bf7cd2faeaf69455e9cf088e9d9e5cff80
Dmitriy Samsonov <LeonSpace777@yandex.ru>
2023-02-04 22:09:35 +0300

    Task2 mod added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,3 +8,44 @@
 // 8 4 2 4
 
 // 17 -> такого числа в массиве нет
+
+System.Console.WriteLine("Введите индекс строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите индекс столбца: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[3, 4];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1))
+{
+     Console.WriteLine(numbers[rows, colums]);
+}
+else 
+{
+    Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
