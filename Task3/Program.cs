fec9727afcf70d6efedfdef5319332ab8f3e97e5
Dmitriy Samsonov <LeonSpace777@yandex.ru>
2023-02-04 22:43:20 +0300

    Task3 mod added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,3 +5,57 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+System.Console.WriteLine("Введите индекс строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите индекс столбца: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[3, 4];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+double[] arr = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    arr[i] = result / numbers.GetLength(0);
+}
+PrintArray(arr);
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
