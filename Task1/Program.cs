ef4d62d65e03db47907c8ef98f6b2422d849fb17
Dmitriy Samsonov <LeonSpace777@yandex.ru>
2023-02-04 15:22:10 +0300

    Task1 mod added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,35 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, n];
+FillArray2D(matrix);
+PrintArray2D(matrix);
